7d8629dea9f645af0ea395bbfd94b949e2ae48b7
GaizkaDM <gaizkaDm@gmail.com>
2025-11-13 16:07:04 +0100

    Fix corregidos imports sin usar

--- javafx_front/src/main/java/com/gaizkaFrost/Main.java
+++ javafx_front/src/main/java/com/gaizkaFrost/Main.java
@@ -4,19 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
-import static javafx.application.Application.launch;
-
-/**
- * Hello world!
- *
- */
 public class Main extends Application
 {
     @Override
